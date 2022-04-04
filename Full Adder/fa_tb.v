0f7c313fb306bab99f9cf88003ce5b58902c75db
SushmitaHubballi <59868892+SushmitaHubballi@users.noreply.github.com>
2022-04-04 13:59:43 +0530

    test-bench

---
+++ Full Adder/fa_tb.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:11:04 04/04/2022
+// Design Name:   fa
+// Module Name:   D:/xilinx_college/full_adder/fa_tb.v
+// Project Name:  full_adder
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: fa
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fa_tb;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg cin;
+
+	// Outputs
+	wire sum;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	fa uut (
+		.sum(sum), 
+		.cout(cout), 
+		.a(a), 
+		.b(b), 
+		.cin(cin)
+	);
+
+	initial begin
+	$monitor("a =%b b =%b sum =%b cin =%b cout =%b ",a,b,sum,cin,cout);
+
+a=0;
+b=1;
+cin=1;
+#5
+a=1;
+b=0;
+cin=1;
+#5
+a=1;
+b=1;
+cin=1;
+	end
+      
+endmodule
+
